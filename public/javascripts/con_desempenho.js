fe7edda237e355121c5aa2342c10410af65c0dff
miguel angel zamora montecinos <mzmprog@gmail.com>
2017-05-13 22:37:32 -0300

    Se actualiza la plantilla a la final .

---
+++ public/javascripts/con_desempenho.js
@@ -0,0 +1,10 @@
+/*+++++++++++++++++++++++++++++++++++*/
+$(document).ready(function() 
+	{
+		$('.pasar').click(function() { return !$('#origen option:selected').remove().appendTo('#destino'); });  
+		$('.quitar').click(function() { return !$('#destino option:selected').remove().appendTo('#origen'); });
+		$('.pasartodos').click(function() { $('#origen option').each(function() { $(this).remove().appendTo('#destino'); }); });
+		$('.quitartodos').click(function() { $('#destino option').each(function() { $(this).remove().appendTo('#origen'); }); });
+		$('.submit').click(function() { $('#destino option').prop('selected', 'selected'); });
+
+}); 
